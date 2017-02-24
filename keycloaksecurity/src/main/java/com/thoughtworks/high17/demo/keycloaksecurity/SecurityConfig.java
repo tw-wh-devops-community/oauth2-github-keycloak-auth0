c59787a9a72881c061a1939c60db1e79884de551
weihongwei <hwwei@thoughtworks.com>
2017-02-24 16:33:13 +0800

    add code comment to explain csrf

--- keycloaksecurity/src/main/java/com/thoughtworks/high17/demo/keycloaksecurity/SecurityConfig.java
+++ keycloaksecurity/src/main/java/com/thoughtworks/high17/demo/keycloaksecurity/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -39,6 +40,12 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
+    /**
+     * if you want to use csrf,you can use code follow.
+     * http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+     * @param http
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
