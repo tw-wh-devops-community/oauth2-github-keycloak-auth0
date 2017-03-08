f6df534af42b3efebaa6d30f76b3b664578c01ad
weihongwei <hwwei@thoughtworks.com>
2017-03-08 17:39:51 +0800

    add cors support

---
+++ springboot-okta/src/main/java/com/thoughtworks/demo/okta/WebConfig.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.demo.okta;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
